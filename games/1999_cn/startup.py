04fe4b7165aa9924edef56fa97c4526711bab0fa
cel-ti <96926560+cel-ti@users.noreply.github.com>
2025-08-24 09:24:21 -0700

    update

---
+++ games/1999_cn/startup.py
@@ -0,0 +1,5 @@
+from dauto.utils.reldplay import start_ldinstance
+from dauto.utils.scoop import start_auto_tool
+
+start_ldinstance("1999_cn")
+start_auto_tool("1999_cn", "MFAAvalonia.exe")
---
+++ games/arknights/startup.py
@@ -0,0 +1,5 @@
+from dauto.utils.reldplay import start_ldinstance
+from dauto.utils.scoop import start_auto_tool
+
+start_ldinstance("arknights")
+start_auto_tool("arknights", "MAA.exe")
