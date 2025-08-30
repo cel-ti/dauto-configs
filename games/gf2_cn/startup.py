8132c55c24eb27a0062589ea0a0abb071e60fa79
cel-ti <96926560+cel-ti@users.noreply.github.com>
2025-08-29 18:06:06 -0700

    update struct

---
+++ games/gf2_cn/startup.py
@@ -0,0 +1,7 @@
+
+from dauto.utils.reldplay import start_ldinstance
+from dauto.utils.scoop import start_auto_tool
+
+start_ldinstance("gf2_cn")
+
+start_auto_tool("gf2_cn", "MaaGF2Exilium.exe")
---
+++ games/gf2_gl/startup.py
@@ -0,0 +1,6 @@
+from dauto.utils.reldplay import start_ldinstance
+from dauto.utils.scoop import start_auto_tool
+
+start_ldinstance("gf2_gl")
+
+start_auto_tool("gf2_gl", "MaaGF2Exilium.exe")
